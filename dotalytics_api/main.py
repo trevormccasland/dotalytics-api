7c3c3be3920b5ac419ac6c74b6fb7fcc7b85c0bf
Tin Lam <tin@irrational.io>
2022-10-02 17:33:18 -0500

    chore: refactor
    Prior to tackling #10, it may be worthwhile to clean up the index-based
    looping structures so the loop is more pythonic.
    
    Signed-off-by: Tin Lam <tin@irrational.io>

--- dotalytics_api/main.py
+++ dotalytics_api/main.py
@@ -11,18 +11,24 @@ import uvicorn
 from dotalytics_api import client
 from dotalytics_api.types import player
 
-app = FastAPI(middleware=[
-    Middleware(
-        CORSMiddleware,
-        allow_origins=['*'],
-        allow_credentials=True,
-        allow_methods=['*'],
-        allow_headers=['*']
-    )
-])
+app = FastAPI(
+    middleware=[
+        Middleware(
+            CORSMiddleware,
+            allow_origins=["*"],
+            allow_credentials=True,
+            allow_methods=["*"],
+            allow_headers=["*"],
+        )
+    ]
+)
 
 
+<<<<<<< HEAD
 async def get_hero_map() -> Dict:
+=======
+def get_heroes_map() -> Dict:
+>>>>>>> 3aba7ff (clean up indices)
     try:
         res = (await client.get_heroes()).result.heroes
         return {r.id: r.name for r in res}
@@ -31,9 +37,17 @@ async def get_hero_map() -> Dict:
                             detail="Bad request %s" % error)
 
 
+def get_items_map() -> Dict:
+    try:
+        res = client.get_game_items().result.items
+        return {r.id: r.name for r in res}
+    except Exception:
+        raise HTTPException(status_code=HTTPStatus.BAD_REQUEST, detail="Bad request")
+
+
 @app.get("/healthz")
 def root():
-    return {"message": 'up and running'}
+    return {"message": "up and running"}
 
 
 @app.get("/matches")
@@ -41,10 +55,15 @@ async def get_matches(account_id: str, matches_requested: int = 5):
 
     match_history = None
     matches = None
-    heroes = None
-    items = None
 
+<<<<<<< HEAD
     hero_map = await get_hero_map()
+=======
+    hero_map = get_heroes_map()
+    item_map = get_items_map()
+
+    print(item_map)
+>>>>>>> 3aba7ff (clean up indices)
 
     try:
         match_history = await client.get_match_history(
@@ -53,112 +72,35 @@ async def get_matches(account_id: str, matches_requested: int = 5):
         matches = [
             (await client.get_match_details(match.match_id)).result for match in match_history.result.matches
         ]
+<<<<<<< HEAD
         heroes, items = await asyncio.gather(client.get_heroes(), client.get_game_items())
         heroes = heroes.result
         items = items.result.items
     except Exception as error:
         raise HTTPException(status_code=HTTPStatus.BAD_REQUEST,
                             detail="Bad request %s" % error)
+=======
 
-    for m in range(len(matches)):
-        for pb in matches[m].picks_bans:
+    except Exception:
+        raise HTTPException(status_code=HTTPStatus.BAD_REQUEST, detail="Bad request")
+>>>>>>> 3aba7ff (clean up indices)
+
+    for m in matches:
+        for pb in m.picks_bans:
             pb.hero_name = hero_map.get(pb.hero_id)
 
-        for p in range(len(matches[m].players)):
-            item_neutral_name = ''
-            item_neutral = matches[m].players[p].item_neutral
-            item_0 = matches[m].players[p].item_0
-            item_0_name = ''
-            item_1 = matches[m].players[p].item_1
-            item_1_name = ''
-            item_2 = matches[m].players[p].item_2
-            item_2_name = ''
-            item_3 = matches[m].players[p].item_3
-            item_3_name = ''
-            item_4 = matches[m].players[p].item_4
-            item_4_name = ''
-            item_5 = matches[m].players[p].item_5
-            item_5_name = ''
-            backpack_0 = matches[m].players[p].backpack_0
-            backpack_0_name = ''
-            backpack_1 = matches[m].players[p].backpack_1
-            backpack_1_name = ''
-            backpack_2 = matches[m].players[p].backpack_2
-            backpack_2_name = ''
-            for item in items:
-                if item_neutral == item.id:
-                    item_neutral_name = item.name
-                if item_0 == item.id:
-                    item_0_name = item.name
-                elif item_1 == item.id:
-                    item_1_name = item.name
-                elif item_2 == item.id:
-                    item_2_name = item.name
-                elif item_3 == item.id:
-                    item_3_name = item.name
-                elif item_4 == item.id:
-                    item_4_name = item.name
-                elif item_5 == item.id:
-                    item_5_name = item.name
-                elif backpack_0 == item.id:
-                    backpack_0_name = item.name
-                elif backpack_1 == item.id:
-                    backpack_1_name = item.name
-                elif backpack_2 == item.id:
-                    backpack_2_name = item.name
-
-            for hero in heroes.heroes:
-                if hero.id == matches[m].players[p].hero_id:
-                    matches[m].players[p] = player.Player(
-                        player_slot=matches[m].players[p].player_slot,
-                        team_number=matches[m].players[p].team_number,
-                        hero_id=matches[m].players[p].hero_id,
-                        item_0=matches[m].players[p].item_0,
-                        item_1=matches[m].players[p].item_1,
-                        item_2=matches[m].players[p].item_2,
-                        item_3=matches[m].players[p].item_3,
-                        item_4=matches[m].players[p].item_4,
-                        item_5=matches[m].players[p].item_5,
-                        item_neutral=matches[m].players[p].item_neutral,
-                        backpack_0=matches[m].players[p].backpack_0,
-                        backpack_1=matches[m].players[p].backpack_1,
-                        backpack_2=matches[m].players[p].backpack_2,
-                        item_netral=matches[m].players[p].item_neutral,
-                        kills=matches[m].players[p].kills,
-                        deaths=matches[m].players[p].deaths,
-                        assists=matches[m].players[p].assists,
-                        leaver_status=matches[m].players[p].leaver_status,
-                        last_hits=matches[m].players[p].last_hits,
-                        denies=matches[m].players[p].denies,
-                        gold_per_min=matches[m].players[p].gold_per_min,
-                        xp_per_min=matches[m].players[p].xp_per_min,
-                        level=matches[m].players[p].level,
-                        net_worth=matches[m].players[p].net_worth,
-                        aghanims_scepter=matches[m].players[p].aghanims_scepter,
-                        aghanims_shard=matches[m].players[p].aghanims_shard,
-                        moonshard=matches[m].players[p].moonshard,
-                        hero_damage=matches[m].players[p].hero_damage,
-                        tower_damage=matches[m].players[p].tower_damage,
-                        hero_healing=matches[m].players[p].hero_healing,
-                        gold=matches[m].players[p].gold,
-                        gold_spent=matches[m].players[p].gold_spent,
-                        scaled_hero_damage=matches[m].players[p].scaled_hero_damage,
-                        scaled_tower_damage=matches[m].players[p].scaled_tower_damage,
-                        scaled_hero_healing=matches[m].players[p].scaled_hero_healing,
-                        item_neutral_name=item_neutral_name,
-                        hero_name=hero.name,
-                        item_0_name=item_0_name,
-                        item_1_name=item_1_name,
-                        item_2_name=item_2_name,
-                        item_3_name=item_3_name,
-                        item_4_name=item_4_name,
-                        item_5_name=item_5_name,
-                        backpack_0_name=backpack_0_name,
-                        backpack_1_name=backpack_1_name,
-                        backpack_2_name=backpack_2_name,
-                    )
-                    break
-
+        for p in m.players:
+            p.hero_name = hero_map.get(p.hero_id)
+            p.item_neutral_name = item_map.get(p.item_neutral)
+            p.item_0_name = item_map.get(p.item_0)
+            p.item_1_name = item_map.get(p.item_1)
+            p.item_2_name = item_map.get(p.item_2)
+            p.item_3_name = item_map.get(p.item_3)
+            p.item_4_name = item_map.get(p.item_4)
+            p.item_5_name = item_map.get(p.item_5)
+            p.backpack_0_name = item_map.get(p.backpack_0)
+            p.backpack_1_name = item_map.get(p.backpack_1)
+            p.backpack_2_name = item_map.get(p.backpack_2)
     return matches
 
 
